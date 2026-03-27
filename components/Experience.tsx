9c44b8e95cab6946f30f6231cdfc93a1230b88c8
Alif Nur <alifnur@192.168.1.5>
2026-03-27 21:09:06 +0700

    add education section

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 export default function Experience() {
   const [activeExp, setActiveExp] = useState<number | null>(null);
   const [activeOrg, setActiveOrg] = useState<number | null>(null);
+  const [activeEdu, setActiveEdu] = useState<number | null>(null);
 
   const experiences = [
     {
@@ -42,6 +43,15 @@ export default function Experience() {
       },
   ];
 
+  const education = [
+    {
+      company: "Brawijaya University",
+      date: "2022 – 2026 (Expected) · 4 Years",
+      role: "Bachelor of Education in Information Technology Education",
+      desc: "GPA 3.85/4.0",
+    },
+  ];
+
     
 
   return (
@@ -49,19 +59,19 @@ export default function Experience() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-24">
 
         {/* HEADER */}
-        <div className="text-center mb-12">
+        <div className="text-center mb-8">
           <div className="flex justify-center items-center gap-2 mb-3">
           <div className="w-7 h-1 bg-primary"></div>
             <p className="text-sm font-semibold">Experience</p>
           </div>
 
-          <h2 className="text-3xl md:text-3xl font-semibold">
+          <h2 className="text-3xl md:text-3xl font-medium">
             My Experience
           </h2>
         </div>
 
         {/* CONTENT */}
-        <div className="relative mb-32">
+        <div className="relative mb-24">
 
           {/* LINE */}
           <div className=" md:block absolute left-4 md:left-1/2 md:-translate-x-1/2 w-[2px] top-0 md:top-10 bottom-0 bg-white/20"></div>
@@ -79,7 +89,12 @@ export default function Experience() {
                   <h3 className="text-2xl font-medium">
                     {item.company}
                   </h3>
-                  <p className="text-sm text-gray-400 mt-2">
+                  <p className={` transition-all duration-300
+                      ${activeExp === index 
+                        ? "text-white text-sm leading-relaxed" 
+                        : "text-gray-400 text-sm leading-relaxed"
+                      }
+                    `}>
                     {item.date}
                   </p>
                 </div>
@@ -89,7 +104,12 @@ export default function Experience() {
                   <h3 className="text-2xl font-medium mb-3">
                     {item.role}
                   </h3>
-                  <p className="text-gray-400 text-sm leading-relaxed">
+                  <p className={` transition-all duration-300
+                      ${activeExp === index 
+                        ? "text-white text-sm leading-relaxed" 
+                        : "text-gray-400 text-sm leading-relaxed"
+                      }
+                    `}>
                     {item.desc}
                   </p>
                 </div>
@@ -114,14 +134,14 @@ export default function Experience() {
         </div>
 
         {/* HEADER */}
-        <div className="text-center mb-12">
+        <div className="text-center mb-8">
 
-          <h2 className="text-3xl md:text-3xl font-semibold">
+          <h2 className="text-3xl md:text-3xl font-medium">
             Organization
           </h2>
         </div>
 
-        <div className="relative">
+        <div className="relative mb-24">
 
           {/* LINE */}
           <div className="md:block absolute left-4 md:left-1/2 md:-translate-x-1/2 w-[2px] top-0 md:top-10 bottom-0 bg-white/20"></div>
@@ -139,7 +159,12 @@ export default function Experience() {
                   <h3 className="text-2xl font-medium">
                     {item.company}
                   </h3>
-                  <p className="text-sm text-gray-400 mt-2">
+                  <p className={` transition-all duration-300
+                      ${activeOrg === index 
+                        ? "text-white text-sm leading-relaxed" 
+                        : "text-gray-400 text-sm leading-relaxed"
+                      }
+                    `}>
                     {item.date}
                   </p>
                 </div>
@@ -149,7 +174,12 @@ export default function Experience() {
                   <h3 className="text-2xl font-medium mb-3">
                     {item.role}
                   </h3>
-                  <p className="text-gray-400 text-sm leading-relaxed">
+                  <p className={` transition-all duration-300
+                      ${activeOrg === index 
+                        ? "text-white text-sm leading-relaxed" 
+                        : "text-gray-400 text-sm leading-relaxed"
+                      }
+                    `}>
                     {item.desc}
                   </p>
                 </div>
@@ -161,6 +191,76 @@ export default function Experience() {
                       w-4 h-4 rounded-full border-4 border-white/20
                       transition-all duration-300
                       ${activeOrg === index 
+                        ? "bg-primary scale-120" 
+                        : "bg-white border-white/20 "
+                      }
+                    `}
+                  ></div>
+                </div>
+
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* HEADER */}
+        <div className="text-center mb-8">
+
+          <h2 className="text-3xl md:text-3xl font-medium">
+            Education
+          </h2>
+        </div>
+
+        <div className="relative">
+
+          {/* LINE */}
+          <div className="md:block absolute left-4 md:left-1/2 md:-translate-x-1/2 w-[2px] top-0 md:top-10 bottom-0 bg-white/20"></div>
+
+          <div className="flex flex-col gap-20">
+            {education.map((item, index) => (
+              <div
+                key={index}
+                onMouseEnter={() => setActiveEdu(index)}
+                onMouseLeave={() => setActiveEdu(null)}
+                className="grid md:grid-cols-2 gap-10 pl-10 md:pl-0  items-center"
+              >
+                {/* LEFT */}
+                <div className="reveal-item text-left md:text-left md:pr-16">
+                  <h3 className="text-2xl font-medium">
+                    {item.company}
+                  </h3>
+                  <p className={`transition-all duration-300
+                      ${activeEdu === index 
+                        ? "text-white text-sm leading-relaxed" 
+                        : "text-gray-400 text-sm leading-relaxed"
+                      }
+                    `}>
+                    {item.date}
+                  </p>
+                </div>
+
+                {/* RIGHT */}
+                <div className="reveal-item md:pl-16">
+                  <h3 className="text-2xl font-medium mb-3">
+                    {item.role}
+                  </h3>
+                  <p className={`transition-all duration-300
+                      ${activeEdu === index 
+                        ? "text-white text-sm leading-relaxed" 
+                        : "text-gray-400 text-sm leading-relaxed"
+                      }
+                    `}>
+                    {item.desc}
+                  </p>
+                </div>
+
+                {/* DOT */}
+                <div className="flex absolute left-2 md:left-1/2  md:-translate-x-1/2">
+                  <div
+                    className={`
+                      w-4 h-4 rounded-full border-4 border-white/20
+                      transition-all duration-300
+                      ${activeEdu === index 
                         ? "bg-primary scale-120" 
                         : "bg-white border-white/20 "
                       }
